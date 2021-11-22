52d6dcf4c2d1ea16eb343a2e43dbebb9b63ac7dc
kisaak <kisaak@sfu.ca>
2021-11-21 20:06:21 -0800

    BFS now checks row constraints

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -10,12 +10,17 @@ def BreadthFirstSearch(puzzle):
     emptyCellIndex = [0, 0]
 
     while puzzleSolved == False:
-        emptyCellIndex = getEmptyCell(puzzle)
+        emptyCellIndex = getEmptyCell(filledPuzzle)
+        currentOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         if emptyCellIndex == [-1, -1]:
             puzzleSolved == True
             break
 
-        filledPuzzle[emptyCellIndex[0], emptyCellIndex[1]] = 99
+        for element in filledPuzzle[emptyCellIndex[0]]:
+            if element != 0:
+                currentOptions.remove(element)
+
+        filledPuzzle[emptyCellIndex[0], emptyCellIndex[1]] = currentOptions[0]
 
     return filledPuzzle
